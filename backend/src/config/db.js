d907ed230668db448a41b9d924e6dc13c9f2d606
CHU YUN HSUAN <angela06612@gmail.com>
2025-11-04 16:12:32 +0800

    db.js檔案修改

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'your_password',  // 暫時使用預設密碼
-  database: process.env.DB_NAME || 'my_diary_app',
+  port: process.env.DB_PORT || 3306,  //檢查這裡
+  database: process.env.DB_DATABASE || 'my_diary_app',
   charset: 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
