6090ca9af4b2fc96d5ac753165ec5e5e3cdc1550
lizrong1017@gmail.com <lizrong1017@gmail.com>
2025-11-16 16:10:24 +0800

    後台系統公告功能

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -28,6 +28,9 @@ router.delete('/diaries/:diaryId', adminController.deleteDiary);
 router.get('/announcements', announcementController.listAll);
 router.post('/announcements', announcementController.create);
 router.delete('/announcements/:id', announcementController.delete);
+// 取得單一公告、更新公告 (編輯)
+router.get('/announcements/:id', announcementController.getById);
+router.put('/announcements/:id', announcementController.update);
 
 // Feedback 管理
 router.get('/feedbacks', feedbackController.adminList);
